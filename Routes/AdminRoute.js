e1d1d2457079daf6ffea8090fb5401521668854a
pratik <pratik>
2023-05-25 18:11:51 +0530

    fixed admin route error

--- Routes/AdminRoute.js
+++ Routes/AdminRoute.js
@@ -1,12 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const adminController = require('../controller/AdminController');
-const isAuth = require('../middleware/is-auth');
+const adminController = require("../Controller/AdminController");
+const isAuth = require("../middleware/is-auth");
 
-router.get('/admin/product/:adminId', adminController.getAdminProducts);
-router.post('/admin/addProduct', isAuth, adminController.addProduct);
-router.put('/admin/editProduct/:productId', adminController.editProduct);
-router.delete('/admin/product/:productId', adminController.deleteProduct);
-router.get('/admin/orders', adminController.getAdminOrders);
-router.put('/admin/orderstatus/:orderId', adminController.updateOrderStatus)
+router.get("/admin/product/:adminId", adminController.getAdminProducts);
+router.post("/admin/addProduct", isAuth, adminController.addProduct);
+router.put("/admin/editProduct/:productId", adminController.editProduct);
+router.delete("/admin/product/:productId", adminController.deleteProduct);
+router.get("/admin/orders", adminController.getAdminOrders);
+router.put("/admin/orderstatus/:orderId", adminController.updateOrderStatus);
 module.exports = router;
